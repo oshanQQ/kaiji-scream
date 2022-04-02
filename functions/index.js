c4b071ad93a1ed1d7c5b791e2598c5d79a9cc573
oshanQQ <oshanqq@gmail.com>
2022-04-02 14:44:28 +0900

    🚚 Rename the function

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,11 @@ const line = require("@line/bot-sdk");
 require("dotenv").config();
 
 const config = {
-    channelAccessToken: functions.config().line.channel_access_token,
-    channelSecret: functions.config().line.channel_secret,
+  channelAccessToken: functions.config().line.channel_access_token,
+  channelSecret: functions.config().line.channel_secret,
 };
 
+exports.kaijiScream = functions.https.onRequest(async (req, res) => {
   const events = req.body.events;
   const client = new line.Client(config);
   const kaijilizedText = kaijilizer(events[0].message.text);
