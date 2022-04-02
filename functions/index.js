8dbb983dab762ee6d1a433b911e74e1c4339d60c
oshanQQ <oshanqq@gmail.com>
2022-04-02 12:01:58 +0900

    📝 Make main code

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
-const functions = require("firebase-functions");
+import * as functions from "firebase-functions";
+import * as line from "@line/bot-sdk";
+import express from "express";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const config = {
+  channelAccessToken: functions.config().linebot.token,
+  channelSecret: functions.config().linebot.secret,
+};
+
+const app = express();
+app.post("/webhooks", line.middleware(config), (request, response) => {
+  return Promise.all(request.body.events.map(handleEvent)).then((result) =>
+    response.json(result)
+  );
+});
+
+const client = new line.Client(config);
+async function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  return replyWithText("メッセージを受け取りました！", event);
+}
+
+async function replyWithText(message, event) {
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: message,
+  });
+}
+
+export const lineBot = functions.https.onRequest(app);
