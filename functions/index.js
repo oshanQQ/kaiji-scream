8cbcac318b080e9efae7410ff86822efd9edcb5b
oshanQQ <oshanqq@gmail.com>
2022-04-02 14:43:29 +0900

    🔧 Separate config to the outside

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,13 @@ const functions = require("firebase-functions");
 const line = require("@line/bot-sdk");
 require("dotenv").config();
 
-exports.helloWorld = functions.https.onRequest(async (req, res) => {
-  const events = req.body.events;
-  const client = new line.Client({
+const config = {
     channelAccessToken: functions.config().line.channel_access_token,
     channelSecret: functions.config().line.channel_secret,
-  });
+};
+
+  const events = req.body.events;
+  const client = new line.Client(config);
   const kaijilizedText = kaijilizer(events[0].message.text);
   const result = await client.replyMessage(events[0].replyToken, {
     type: "text",
